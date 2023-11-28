c2cb7206c5322d4d07da8ba4754e71e0def11b42
Oscar Barajas <oscar@gndx.dev>
2023-11-28 10:56:10 -0500

    :sparkles: [ADD] FIX OG

--- src/utils/createOgImage.ts
+++ src/utils/createOgImage.ts
@@ -10,19 +10,19 @@ export const createOgImage = ({
     // Add your own Cloudinary account ID.
     `https://res.cloudinary.com/gndxdev/image/upload`,
     // Composed Image Transformations
-    `w_1600,h_836,q_100`,
+    `w_960,h_502,q_70`,
     // TITLE
     // Karla google font in light rose
-    `l_text:Ubuntu_92_bold:${e(title)},co_rgb:ffe4e6,c_fit,w_1200,h_400`,
+    `l_text:Ubuntu_56_bold:${e(title)},co_rgb:ffe4e6,c_fit,w_750,h_100`,
     // Positioning
-    `fl_layer_apply,g_south_west,x_100,y_340`,
+    `fl_layer_apply,g_south_west,x_100,y_240`,
     // META
     // Karla, but smaller
-    `l_text:Ubuntu_52_bold:${e(meta)},co_rgb:ffe4e680,c_fit,w_1400`,
+    `l_text:Ubuntu_32_bold:${e(meta)},co_rgb:ffe4e680,c_fit,w_500`,
     // Positioning
     `fl_layer_apply,g_south_west,x_100,y_100`,
     // BG
-    `og-base-gndx-purple_efdec6.png`,
+    `gndx-dev-og-2024.png`,
   ].join("/");
 
 // double escape for commas and slashes
